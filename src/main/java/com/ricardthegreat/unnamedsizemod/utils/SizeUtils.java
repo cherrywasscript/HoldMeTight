612f137a3203814459fb8a4c0cd4eed0d75311c1
cherry <cherrywasscript@gmail.com>
2025-02-09 18:32:18 +0000

    changed sizeutils to allow for custom size change durations, other files edited to account for the change in method names

--- src/main/java/com/ricardthegreat/unnamedsizemod/utils/SizeUtils.java
+++ src/main/java/com/ricardthegreat/unnamedsizemod/utils/SizeUtils.java
@@ -16,44 +16,60 @@ public class SizeUtils {
     private static Float maxScale = 8.0f;
 
     
-    public static void setSize() {
+    public static void setSizeInstant() {
         
     }
 
-    public static void setTargetSize(Entity entity, Float size) {
-        checkMaxHitbox(entity, size);
-        getScaleData(entity).setTargetScale(size);
-    }   
-
-    public static void multTargetSize(Entity entity, Float size){
-        ScaleData data = getScaleData(entity);
-
-        Float targetScale = data.getTargetScale()*size;
-
-        data.setTargetScale(targetScale);
+    public static void multSizeInstant() {
+        
     }
 
+    //i should probably grab the actual default from pekhui as if it changes from 20 this wont however a second is a good default i feel
+    public static void setSizeOverTimeDefault(Entity entity, Float size) {
+        checkMaxHitbox(entity, size, 20);
+        ScaleData data = getScaleData(entity);
+        data.setScaleTickDelay(20);
+        data.setTargetScale(size);
+    }   
+
+    public static void multSizeOverTimeDefault(Entity entity, Float size){
+        Float targetScale = getScaleData(entity).getTargetScale()*size;
+        setSizeOverTimeDefault(entity, targetScale);
+    }
+
+    public static void setSizeOverTimeCustom(Entity entity, Float size, int ticks) {
+        checkMaxHitbox(entity, size, ticks);
+        ScaleData data = getScaleData(entity);
+        data.setScaleTickDelay(ticks);
+        data.setTargetScale(size);
+    }   
+
+    public static void multSizeOverTimeCustom(Entity entity, Float size, int ticks){
+        Float targetScale = getScaleData(entity).getTargetScale()*size;
+        setSizeOverTimeCustom(entity, targetScale, ticks);
+    }
+
+
+
     public static float getSize(Entity entity) {
-
         return getScaleData(entity).getScale();
-
     }
 
     private static ScaleData getScaleData(Entity entity) {
-
         PehkuiEntityExtensions pEnt = (PehkuiEntityExtensions) entity;
-
         ScaleData data = pEnt.pehkui_getScaleData(base);
-
         return data;
     }
 
-    private static void checkMaxHitbox(Entity entity, float size) {
+    private static void checkMaxHitbox(Entity entity, float size, int ticks) {
 
         PehkuiEntityExtensions pEnt = (PehkuiEntityExtensions) entity;
 
         ScaleData heightData = pEnt.pehkui_getScaleData(hitbox_height);
         ScaleData widthData = pEnt.pehkui_getScaleData(hitbox_width);
+
+        heightData.setScaleTickDelay(ticks);
+        widthData.setScaleTickDelay(ticks);
 
         if (size > maxScale) {
             heightData.setTargetScale(maxScale/size);
@@ -66,17 +82,6 @@ public class SizeUtils {
         System.out.println("Target: " + heightData.getTargetScale());
         System.out.println("actual" + heightData.getScale());
 
-        
-
         //return data;
-    }
-
-
-    private static Float limitToMax(Float scale) {
-        if(scale > maxScale){
-            return maxScale;
-        }else {
-            return scale;
-        }
     }
 }
