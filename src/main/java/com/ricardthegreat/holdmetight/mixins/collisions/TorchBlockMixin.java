21eeab11b8dbf787d3818db90b74a47d61c08927
cherry <cherrywasscript@gmail.com>
2025-03-04 18:54:57 +0000

    added fire damage when standing on torch like campfire

--- src/main/java/com/ricardthegreat/holdmetight/mixins/collisions/TorchBlockMixin.java
+++ src/main/java/com/ricardthegreat/holdmetight/mixins/collisions/TorchBlockMixin.java
@@ -6,10 +6,17 @@ import com.ricardthegreat.holdmetight.utils.SizeUtils;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.CampfireBlock;
 import net.minecraft.world.level.block.TorchBlock;
+import net.minecraft.world.level.block.entity.CampfireBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.EntityCollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -32,11 +39,45 @@ public class TorchBlockMixin extends Block{
             EntityCollisionContext entContext = (EntityCollisionContext) context;
             Entity ent = entContext.getEntity();
             if(ent != null && SizeUtils.getSize(ent) < 0.21){
-                System.out.println(voxelshape);
                 return voxelshape;
             }
         }
-        
         return Shapes.empty();
+    }
+
+    @Override
+    public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
+        
+        if (entity instanceof LivingEntity && !EnchantmentHelper.hasFrostWalker((LivingEntity)entity) && entOnTop(entity, pos) && SizeUtils.getSize(entity) < 0.21) {
+            if (state.is(Blocks.TORCH)) {
+                entity.hurt(level.damageSources().inFire(), 1);
+            }else if (state.is(Blocks.SOUL_TORCH)) {
+                entity.hurt(level.damageSources().inFire(), 2);
+            }
+            
+        }
+    }
+
+    private boolean entOnTop(Entity entity, BlockPos pos){
+        Vec3 entPos = entity.position();
+        //System.out.println("line 64/"+ pos);
+        if (entPos.y() == pos.getY()+0.625 && correctX(entPos.x(), pos.getX()) && correctZ(entPos.z(), pos.getZ())) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean correctX(double entX, double blockX){
+        if (entX > blockX + 0.4375 && entX < blockX + 0.5625) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean correctZ(double entZ, double blockZ){
+        if (entZ > blockZ + 0.4375 &&entZ < blockZ + 0.5625) {
+            return true;
+        }
+        return false;
     }
 }
