39ae2300b5684357b3e9afe58ab5def67c543cd6
cherry <cherrywasscript@gmail.com>
2025-03-04 12:17:20 +0000

    added fence gate collision params

---
+++ src/main/java/com/ricardthegreat/holdmetight/mixins/collisions/FenceGateBlockMixin.java
@@ -0,0 +1,37 @@
+package com.ricardthegreat.holdmetight.mixins.collisions;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.ricardthegreat.holdmetight.utils.SizeUtils;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.FenceGateBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.EntityCollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+@Mixin(FenceGateBlock.class)
+public class FenceGateBlockMixin {
+
+
+    //treats fence gates as open for any ent under 0.21 scale
+    //wanted to do <= 0.2 but i think 0.2 in game is like 0.20000000001 or something bc it didnt work
+    @Inject(method = "getCollisionShape(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;", at = @At("HEAD"), cancellable = true)
+    public void getCollisionShape(BlockState p_53396_, BlockGetter p_53397_, BlockPos p_53398_, CollisionContext context, CallbackInfoReturnable<VoxelShape> cir) {
+        if (context instanceof EntityCollisionContext) {
+            EntityCollisionContext entContext = (EntityCollisionContext) context;
+
+            Entity ent = entContext.getEntity();
+            if(ent != null && SizeUtils.getSize(ent) < 0.21){
+                cir.setReturnValue(Shapes.empty());
+            }
+        }
+    }
+}
