0a0fae53ba32641566808d95a7086ef5728506af
cherry <cherrywasscript@gmail.com>
2025-03-04 12:05:53 +0000

    fence post collision only works for smaller ents now

--- src/main/java/com/ricardthegreat/holdmetight/mixins/CrossCollisionBlockMixin.java
+++ src/main/java/com/ricardthegreat/holdmetight/mixins/CrossCollisionBlockMixin.java
@@ -1,26 +1,46 @@
 package com.ricardthegreat.holdmetight.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.ricardthegreat.holdmetight.utils.SizeUtils;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.CrossCollisionBlock;
+import net.minecraft.world.level.block.FenceBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.EntityCollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 @Mixin(CrossCollisionBlock.class)
 public class CrossCollisionBlockMixin {
-
-    @Shadow
-    VoxelShape[] collisionShapeByIndex;
     
-    @ModifyReturnValue(method = "getCollisionShape(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;", at = @At("RETURN"))
-    public VoxelShape getCollisionShape(VoxelShape original) {
-        return this.collisionShapeByIndex[0];
-    }
+   @Shadow
+   VoxelShape[] collisionShapeByIndex;
+
+   @Shadow
+   protected int getAABBIndex(BlockState p_52364_) {return 0;}
+
+
+   //not sure if this works serverside yet but it should
+   //returns only the post hitbox for fences if the player is less than 0.21
+   //wanted to do <= 0.2 but i think 0.2 in game is like 0.20000000001 or something bc it didnt work
+   @Overwrite
+   public VoxelShape getCollisionShape(BlockState p_52357_, BlockGetter p_52358_, BlockPos p_52359_, CollisionContext context) {
+      if (context instanceof EntityCollisionContext) {
+         EntityCollisionContext entContext = (EntityCollisionContext) context;
+
+         Entity ent = entContext.getEntity();
+         if(ent != null && SizeUtils.getSize(ent) < 0.21){
+            //System.out.println(ent.getName() + "/" + SizeUtils.getSize(ent));
+            return this.collisionShapeByIndex[0];
+         }
+      }
+
+      return this.collisionShapeByIndex[this.getAABBIndex(p_52357_)];
+   }
 }
