2f6e2a743733bae8653383b632231da0e929f5cb
cherry <cherrywasscript@gmail.com>
2025-03-24 15:10:57 +0000

    moved to a folder for sorting or something

--- src/main/java/com/ricardthegreat/holdmetight/mixins/playerextensions/PlayerMixin.java
+++ src/main/java/com/ricardthegreat/holdmetight/mixins/playerextensions/PlayerMixin.java
@@ -1,4 +1,4 @@
-package com.ricardthegreat.holdmetight.mixins;
+package com.ricardthegreat.holdmetight.mixins.playerextensions;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
