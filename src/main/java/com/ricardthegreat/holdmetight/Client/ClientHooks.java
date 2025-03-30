ac2bae6ae7c5a3344902a044cfd30d99e67cdb52
cherry <cherrywasscript@gmail.com>
2025-03-30 21:39:15 +0100

    updated to fix removed classes

--- src/main/java/com/ricardthegreat/holdmetight/Client/ClientHooks.java
+++ src/main/java/com/ricardthegreat/holdmetight/Client/ClientHooks.java
@@ -4,36 +4,35 @@ package com.ricardthegreat.holdmetight.Client;
 
 import com.ricardthegreat.holdmetight.Client.screens.CarryPositionScreen;
 import com.ricardthegreat.holdmetight.Client.screens.SizeRayScreen;
+import com.ricardthegreat.holdmetight.Client.screens.remotes.AdvancedSizeRemoteScreen;
+import com.ricardthegreat.holdmetight.Client.screens.remotes.BasicSizeRemoteScreen;
 import com.ricardthegreat.holdmetight.Client.screens.remotes.MasterSizeRemoteScreen;
-import com.ricardthegreat.holdmetight.Client.screens.remotes.overtime.TimedSizeRemoteScreen;
-import com.ricardthegreat.holdmetight.Client.screens.remotes.random.RandomSizeRemoteScreen;
-import com.ricardthegreat.holdmetight.Client.screens.remotes.setmult.CustomSizeRemoteScreen;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 
 public class ClientHooks {
-    public static void openSizeRemoteScreen(Player player, InteractionHand hand) {
-        Minecraft.getInstance().setScreen(new CustomSizeRemoteScreen(player, hand));
-    }
 
-    public static void openRandomRemoteScreen(Player player, InteractionHand hand) {
-        Minecraft.getInstance().setScreen(new RandomSizeRemoteScreen(player, hand));
+    //remote screens
+    public static void openBasicSizeRemoteScreen(Player player, InteractionHand hand){
+        Minecraft.getInstance().setScreen(new BasicSizeRemoteScreen(player, hand));
     }
-
-    public static void openDurationRemoteScreen(Player player, InteractionHand hand) {
-        Minecraft.getInstance().setScreen(new TimedSizeRemoteScreen(player, hand));
+    public static void openAdvancedSizeRemoteScreen(Player player, InteractionHand hand) {
+        Minecraft.getInstance().setScreen(new AdvancedSizeRemoteScreen(player, hand));
     }
-
     public static void openMasterRemoteScreen(Player player, InteractionHand hand) {
         Minecraft.getInstance().setScreen(new MasterSizeRemoteScreen(player, hand));
     }
 
+
+    //ray screens
     public static void openSizeRayScreen(Player player, InteractionHand hand){
         Minecraft.getInstance().setScreen(new SizeRayScreen(player, hand));
     }
 
+
+    //carry pos screen
     public static void openCarryPositionScreen(Player player){
         Minecraft.getInstance().setScreen(new CarryPositionScreen(player));
     }
