781e76ab1cc3041919e2c55226b08f75153ffe37
cherrywasscript <cherrywasscript@gmail.com>
2025-07-04 16:02:27 +0100

    updated so now properly returns true if iblockswimming allows for swimming

--- src/main/java/com/ricardthegreat/holdmetight/mixins/swimming/SwimmingMixin.java
+++ src/main/java/com/ricardthegreat/holdmetight/mixins/swimming/SwimmingMixin.java
@@ -3,8 +3,14 @@ package com.ricardthegreat.holdmetight.mixins.swimming;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.ricardthegreat.holdmetight.utils.IBlockSwimming;
 
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.extensions.IForgeEntity;
 import net.minecraftforge.fluids.FluidType;
 import virtuoel.pehkui.mixin.step_height.IForgeEntityMixin;
@@ -13,18 +19,29 @@ import virtuoel.pehkui.mixin.step_height.IForgeEntityMixin;
 public abstract interface SwimmingMixin{
 
     @Overwrite(remap = false)
-    default boolean canStartSwimming(){
-        System.out.println("test");
+    default boolean canStartSwimming() {
+        if (!this.getEyeInFluidType().isAir() && this.canSwimInFluidType(this.getEyeInFluidType())) {
+            return true;
+        }else if (this instanceof Player) {
+            Entity ent = (Entity) (Object) this;
 
-        return !this.getEyeInFluidType().isAir() && this.canSwimInFluidType(this.getEyeInFluidType());
+            IBlockSwimming swimming = (IBlockSwimming) ent;
+            
+            return swimming.getInSwimmableBlock();
+        }else{
+            return false;
+        }
     }
 
-
+    @Overwrite(remap = false)
+    default boolean canSwimInFluidType(FluidType type) {
+        return type.canSwim(self());
+    }
 
     @Shadow
     abstract FluidType getEyeInFluidType();
 
     @Shadow
-    abstract boolean canSwimInFluidType(FluidType type);
+    abstract Entity self();
 }
 
