87698da866cca659e2b43c89dd11f82588e20549
cherrywasscript <cherrywasscript@gmail.com>
2026-02-03 18:33:10 +0000

    added put down logic

--- src/main/java/com/ricardthegreat/holdmetight/items/PlayerStandinItem.java
+++ src/main/java/com/ricardthegreat/holdmetight/items/PlayerStandinItem.java
@@ -1,22 +1,124 @@
 package com.ricardthegreat.holdmetight.items;
 
-import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.UUID;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.ricardthegreat.holdmetight.init.BlockInit;
+import com.ricardthegreat.holdmetight.init.ItemInit;
+import com.ricardthegreat.holdmetight.network.PacketHandler;
+import com.ricardthegreat.holdmetight.network.clientbound.CUsePlayerItemPacket;
+import com.ricardthegreat.holdmetight.utils.sizeutils.PlayerSizeUtils;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.phys.Vec3;
 
 public class PlayerStandinItem extends Item {
+
+    private static String PLAYER_UUID = "LINKED_PLAYER_UUID";
 
     public PlayerStandinItem(Properties properties) {
         super(properties);
     }
 
     public InteractionResult useOn(@Nonnull UseOnContext context) {
-        Vec3 bp = context.getClickLocation();
+        //get the player using the item and fail if it doesnt exist
+        Player vehicle = context.getPlayer();
+        if (vehicle == null) {
+            System.out.println("no vehicle");
+            return InteractionResult.FAIL;
+        }
 
-        return InteractionResult.PASS;
+        //get the item the player is holding
+        ItemStack item = vehicle.getItemInHand(context.getHand()); 
+
+        //get the tag from the item and fail if it has no tag
+        if (!item.hasTag()) {
+            System.out.println("no tag");
+            return InteractionResult.FAIL;
+        }
+
+        CompoundTag tag = item.getTag();
+
+
+        //get all passengers from the player, this should be temporary as ill probably at some point have "passengers" that are not in the same dimension
+        List<Entity> passengers = vehicle.getPassengers();
+
+        if (passengers.size() == 0) {
+            System.out.println("no passengers");
+            return InteractionResult.FAIL; // fail if there are no passengers
+        }
+
+        //check if item entity is a passenger
+        UUID id = tag.getUUID(PLAYER_UUID);
+        Entity passenger = null;
+        
+        for(Entity entity : passengers){
+            if (entity.getUUID().equals(id)) {
+                passenger = entity;
+            }
+        }
+        
+        if (passenger == null) {
+            System.out.println("passenger null");
+            return InteractionResult.FAIL;
+        }
+
+        //send packet to clients as this should be run serverside
+        //if (!context.getLevel().isClientSide) {
+        //    passenger.dismountTo(context.getClickLocation().x, context.getClickLocation().y, context.getClickLocation().z);
+        //}
+        
+        passenger.stopRiding();
+        passenger.dismountTo(context.getClickLocation().x, context.getClickLocation().y, context.getClickLocation().z);
+            
+        if (vehicle.getMainHandItem().is(this)) {
+            vehicle.getMainHandItem().shrink(1);
+        }else if (vehicle.getOffhandItem().is(this)) {
+            vehicle.getOffhandItem().shrink(1);
+        }
+            
+        return InteractionResult.SUCCESS;
     }
-    
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> list, TooltipFlag flag) {
+        CompoundTag tag = stack.getTag();
+
+        if (level != null) {
+            Player player = level.getPlayerByUUID(tag.getUUID(PLAYER_UUID));
+
+            if (player != null) {
+                list.add(Component.literal("Size: " + PlayerSizeUtils.getSize(player)));
+            }
+        }
+        super.appendHoverText(stack, level, list, flag);
+    }
+
+    public static ItemStack createPlayerItem(Player player){
+        ItemStack item = new ItemStack(ItemInit.PLAYER_ITEM.get());
+
+        item.setHoverName(player.getDisplayName());
+
+        CompoundTag tag = item.getOrCreateTag();
+
+        tag.putUUID(PLAYER_UUID, player.getUUID());
+
+        item.setTag(tag);
+
+        return item;
+    }
 }
