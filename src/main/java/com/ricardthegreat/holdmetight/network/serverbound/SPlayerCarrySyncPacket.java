ea879b4902a7bdf4c3de44a8274757792b149b0a
cherrywasscript <cherrywasscript@gmail.com>
2026-03-20 15:12:22 +0000

    updated to save multiple carry positions at once

--- src/main/java/com/ricardthegreat/holdmetight/network/serverbound/SPlayerCarrySyncPacket.java
+++ src/main/java/com/ricardthegreat/holdmetight/network/serverbound/SPlayerCarrySyncPacket.java
@@ -13,16 +13,19 @@ import com.ricardthegreat.holdmetight.network.clientbound.CPlayerCarrySyncPacket
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.network.NetworkEvent;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
 
 public class SPlayerCarrySyncPacket {
 
     private final ArrayList<CompoundTag> carriedPlayers;
-    private final CarryPosition customPos;
+    private final ArrayList<CarryPosition> customPos;
 
-    public SPlayerCarrySyncPacket(ArrayList<CompoundTag> carriedPlayers, CarryPosition customPos){
+    public SPlayerCarrySyncPacket(ArrayList<CompoundTag> carriedPlayers, ArrayList<CarryPosition> customPos){
         this.carriedPlayers = carriedPlayers;
         this.customPos = customPos;
     }
@@ -34,9 +37,14 @@ public class SPlayerCarrySyncPacket {
         for(int i = 0; i < listSize; i++){
             temp.add(buffer.readNbt());
         }
-
         this.carriedPlayers = temp;
-        this.customPos = new CarryPosition(buffer.readUtf(), buffer.readInt(), buffer.readDouble(), buffer.readDouble(), buffer.readDouble(), buffer.readBoolean());
+
+        listSize = buffer.readInt();
+        ArrayList<CarryPosition> tempPos = new ArrayList<CarryPosition>();
+        for(int i = 0; i < listSize; i++){
+            tempPos.add(new CarryPosition(buffer.readUtf(), buffer.readInt(), buffer.readDouble(), buffer.readDouble(), buffer.readDouble(), buffer.readBoolean()));
+        }
+        this.customPos = tempPos;
     }
 
     public void encode(FriendlyByteBuf buffer){
@@ -46,12 +54,15 @@ public class SPlayerCarrySyncPacket {
             buffer.writeNbt(compoundTag);
         }
 
-        buffer.writeUtf(customPos.posName);
-        buffer.writeInt(customPos.RotationOffset);
-        buffer.writeDouble(customPos.leftRightMove);
-        buffer.writeDouble(customPos.vertOffset);
-        buffer.writeDouble(customPos.xymult);
-        buffer.writeBoolean(customPos.headLink);
+        buffer.writeInt(customPos.size());
+        for (CarryPosition carryPos : customPos) {
+            buffer.writeUtf(carryPos.posName);
+            buffer.writeInt(carryPos.RotationOffset);
+            buffer.writeDouble(carryPos.leftRightMove);
+            buffer.writeDouble(carryPos.vertOffset);
+            buffer.writeDouble(carryPos.xymult);
+            buffer.writeBoolean(carryPos.headLink);
+        }
     }
 
     public void handle(Supplier<NetworkEvent.Context> context){
