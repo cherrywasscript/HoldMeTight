0cbb9a0a7c9ef61680e25b72ef1f49fc182c9156
cherrywasscript <cherrywasscript@gmail.com>
2026-03-21 18:51:12 +0000

    added new mixin to give each carry slot a custom hover tooltip

---
+++ src/main/java/com/ricardthegreat/holdmetight/mixins/curios/CurioHoverTooltip.java
@@ -0,0 +1,56 @@
+package com.ricardthegreat.holdmetight.mixins.curios;
+
+import java.util.Optional;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.ricardthegreat.holdmetight.carry.CarryPosition;
+import com.ricardthegreat.holdmetight.carry.PlayerCarry;
+import com.ricardthegreat.holdmetight.carry.PlayerCarryProvider;
+import com.ricardthegreat.holdmetight.init.ItemInit;
+import com.ricardthegreat.holdmetight.utils.constants.CarryPosConstants;
+
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen;
+import net.minecraft.client.gui.screens.recipebook.RecipeUpdateListener;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.ItemStack;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotResult;
+import top.theillusivec4.curios.api.client.ICuriosScreen;
+import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
+import top.theillusivec4.curios.api.type.inventory.IDynamicStackHandler;
+import top.theillusivec4.curios.client.gui.CuriosScreen;
+import top.theillusivec4.curios.client.gui.CuriosScreenV2;
+import top.theillusivec4.curios.common.inventory.CurioSlot;
+import top.theillusivec4.curios.common.inventory.container.CuriosContainer;
+
+@Mixin(CuriosScreenV2.class)
+public abstract class CurioHoverTooltip extends EffectRenderingInventoryScreen<CuriosContainer> implements RecipeUpdateListener, ICuriosScreen {
+    
+    public CurioHoverTooltip(CuriosContainer p_98701_, Inventory p_98702_, Component p_98703_) {
+        super(p_98701_, p_98702_, p_98703_);
+    }
+
+    @Inject(method = "render(Lnet/minecraft/client/gui/GuiGraphics;IIF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;renderTooltip(Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;II)V"), cancellable = true)
+    private void renderTooltip(GuiGraphics graphics, int x, int y, float partialTicks, CallbackInfo info, @Local CurioSlot curioSlot){
+        if (curioSlot.getIdentifier().equals(CarryPosConstants.CUSTOM)) {
+            if (this.minecraft != null) {
+                if (this.minecraft.player != null) {
+                    PlayerCarry carry = PlayerCarryProvider.getPlayerCarryCapability(this.minecraft.player);
+                    
+                    CarryPosition pos = carry.getCarryPosition(this.minecraft.player, curioSlot.getIdentifier() + curioSlot.getContainerSlot());
+                    graphics.renderTooltip(this.font, Component.literal(pos.posName + " (Custom Carry)"), x, y);
+                    info.cancel();
+                }
+            }
+        }
+    }
+  
+}
